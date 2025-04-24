18cd92d743cee3b7f952e350e93ded1068c9d9d4
G_us <henrygparsons@gmail.com>
2025-04-24 17:23:14 +0200

    Refactor main.py to simplify device discovery and remove unused logging and connection code; add new PCB configuration files.

--- PCApp/main.py
+++ PCApp/main.py
@@ -1,78 +1,9 @@
-"""
-Service Explorer
-----------------
-
-An example showing how to access and print out the services, characteristics and
-descriptors of a connected GATT server.
-
-Created on 2019-03-25 by hbldh <henrik.blidh@nedomkull.com>
-
-"""
-
-import argparse
 import asyncio
-import logging
-
-from bleak import BleakClient, BleakScanner
-
-logger = logging.getLogger(__name__)
-
+from bleak import BleakScanner
 
 async def main():
-    logger.info("starting scan...")
+    devices = await BleakScanner.discover()
+    for d in devices:
+        print(d)
 
-    device = await BleakScanner.find_device_by_name("TappieTest")
-    if device is None:
-        logger.info("device not found")
-        return
-
-    logger.info("connecting to device...")
-
-    async with BleakClient(device) as client:
-        logger.info("connected")
-
-        for service in client.services:
-            logger.info("[Service] %s", service)
-
-            for char in service.characteristics:
-                if "read" in char.properties:
-                    try:
-                        value = await client.read_gatt_char(char.uuid)
-                        extra = f", Value: {value}"
-                    except Exception as e:
-                        extra = f", Error: {e}"
-                else:
-                    extra = ""
-
-                if "write-without-response" in char.properties:
-                    extra += f", Max write w/o rsp size: {char.max_write_without_response_size}"
-
-                logger.info(
-                    "  [Characteristic] %s (%s)%s",
-                    char,
-                    ",".join(char.properties),
-                    extra,
-                )
-
-                for descriptor in char.descriptors:
-                    try:
-                        value = await client.read_gatt_descriptor(descriptor.handle)
-                        logger.info("    [Descriptor] %s, Value: %r", descriptor, value)
-                    except Exception as e:
-                        logger.error("    [Descriptor] %s, Error: %s", descriptor, e)
-
-        logger.info("disconnecting...")
-
-    logger.info("disconnected")
-
-
-if __name__ == "__main__":
-
-
-    log_level = logging.DEBUG
-    logging.basicConfig(
-        level=log_level,
-        format="%(asctime)-15s %(name)-8s %(levelname)s: %(message)s",
-    )
-
-    asyncio.run(main())
+asyncio.run(main())
